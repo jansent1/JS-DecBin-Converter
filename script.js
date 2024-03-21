e5f52078d1d179b4c10b8c66bef9ac8a9dfe1cf0
Teun Jansen <teunjansen91@gmail.com>
2024-03-21 10:44:33 +0100

    BaseCase input 1 bugFix

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ const decimalToBinary = (input) => {
   // Version 3.0 (with recursion):
   if (input === 0) {
     return "0";
+  } else if (input === 1) {
+    return "1";
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
